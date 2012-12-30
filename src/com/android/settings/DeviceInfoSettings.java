5115e8cda0ba280b5943ce02d6b00f30882b0c57
thekraven <savethestyle1@gmail.com>
2012-12-30 06:24:02 -0600

    add partition info
    Change-Id: Iafd1c5edf0e0da2458abaa4dbd130ba446b3edaa

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -32,8 +32,10 @@ import android.provider.Settings;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -81,7 +83,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
 
         setStringSummary(KEY_FIRMWARE_VERSION, Build.VERSION.RELEASE);
         findPreference(KEY_FIRMWARE_VERSION).setEnabled(true);
-        setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
+        setStringSummary(KEY_BASEBAND_VERSION, getCDMAbaseband());
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
@@ -213,6 +215,35 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         }
     }
 
+    private String getCDMAbaseband() {
+        String bband = null;
+        BufferedReader reader = null;
+
+        try {
+            // Grab a reader to /sys/devices/system/soc/soc0/build_id
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream("/sys/devices/system/soc/soc0/build_id")), 1000);
+
+            // Grab the first line from build_id
+             String line = reader.readLine();
+
+            // Split on the colon, we need info to the right of colon
+            bband = line.trim();
+        }
+        catch(IOException io) {
+            io.printStackTrace();
+            // bband = new String[1];
+            bband = "error";
+        }
+        finally {
+            // Make sure the reader is closed no matter what
+            try { reader.close(); }
+            catch(Exception e) {}
+            reader = null;
+        }
+
+        return bband;
+      }
+	
     private String getFormattedKernelVersion() {
         String procVersionStr;
 
